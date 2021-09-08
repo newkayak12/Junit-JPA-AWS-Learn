460b8d15ee6995de263e8343bfe4ed932f810f45
newkayak12 <newkayak12@gmail.com>
2021-09-08 19:35:31 +0900

    scripts

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-ABSPATH = ${readlink -f $0}
-ABSDIR = ${ABSDIR/profile.sh}
+ABSPATH = $(readlink -f $0)
+ABSDIR = $(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 IDLE_PORT = $(find_idle_port)
