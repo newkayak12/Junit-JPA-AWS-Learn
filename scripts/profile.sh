b2d7f6d4d433f17b15b8f4d1d7a9b816884d44af
newkayak12 <newkayak12@gmail.com>
2021-09-08 21:19:05 +0900

    scripts

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,40 +1,82 @@
-#!user/bin/env bash
+#!/usr/bin/env bash
 
-#쉬고 있는 profile 조회
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
-function find_idle_profile(){
-	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-	if [ ${RESPONSE_CODE} -ge 400 ]
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-	then
-		CURRENT_PROFILE = real2
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
 
-	else
-		CURRENT_PROFILE = $(curl -s http://localhost/profile)
+    echo "${IDLE_PROFILE}"
+}
 
-	fi
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
 
-	if[ ${CURRENT_PROFILE} == real1 ]
-	then 
-		IDLE_PROFILE = real2
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
 
-	else 
-		IDLE_PROFILE = real1
+
+
+
+# #!user/bin/env bash
+
+# #쉬고 있는 profile 조회
+
+# function find_idle_profile(){
+# 	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+# 	if [ ${RESPONSE_CODE} -ge 400 ]
+
+# 	then
+# 		CURRENT_PROFILE = real2
+
+# 	else
+# 		CURRENT_PROFILE = $(curl -s http://localhost/profile)
+
+# 	fi
+
+# 	if[ ${CURRENT_PROFILE} == real1 ]
+# 	then 
+# 		IDLE_PROFILE = real2
+
+# 	else 
+# 		IDLE_PROFILE = real1
 	
-	fi
+# 	fi
 
-	echo "${IDLE_PROFILE}"
-}
+# 	echo "${IDLE_PROFILE}"
+# }
 
-function find_idle_port(){
-	IDLE_PROFILE = ${find_idle_profile}
+# function find_idle_port(){
+# 	IDLE_PROFILE = ${find_idle_profile}
 
-	if[ ${IDLE_PROFILE} == real1 ]
-	then
-		echo "8081"
+# 	if[ ${IDLE_PROFILE} == real1 ]
+# 	then
+# 		echo "8081"
 
-	else 
-		echo "8082"
-	fi
-}
+# 	else 
+# 		echo "8082"
+# 	fi
+# }
