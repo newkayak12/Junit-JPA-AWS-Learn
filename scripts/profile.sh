c4c3080b2bf95532b9646b8f0f4c7da1461905ed
newkayak12 <newkayak12@gmail.com>
2021-09-08 19:29:07 +0900

    scripts

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -8,21 +8,19 @@ function find_idle_profile(){
 	if [ ${RESPONSE_CODE} -ge 400 ]
 
 	then
-	CURRENT_PROFILE = real2
+		CURRENT_PROFILE = real2
 
 	else
-
-	CURRENT_PROFILE = $(curl -s http://localhost/profile)
+		CURRENT_PROFILE = $(curl -s http://localhost/profile)
 
 	fi
 
 	if[ ${CURRENT_PROFILE} == real1 ]
 	then 
-	IDLE_PROFILE = real2
+		IDLE_PROFILE = real2
 
 	else 
-
-	IDLE_PROFILE = real1
+		IDLE_PROFILE = real1
 	
 	fi
 
