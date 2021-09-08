1afd2b777fcb28e5901ecbb8c84904b661470214
newkayak12 <newkayak12@gmail.com>
2021-09-08 21:13:46 +0900

    scripts

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,4 +1,46 @@
 #!/usr/bin/env bash
+# ABSPATH=$(readlink -f $0)
+# ABSDIR=$(dirname $ABSPATH)
+# source ${ABSDIR}/profile.sh
+# source ${ABSDIR}/switch.sh
+
+# IDLE_PORT=$(find_idle_port)
+
+# echo "HEALTH CHECK START..."
+# echo "IDLE_PORT : $IDLE_PORT"
+# echo "curl -s http://localhost:$IDLE_PORT/profile"
+
+# sleep 10
+
+# for RETRY_COUNT in {1..10}
+# do
+# 	RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
+# 	UP_COUNT=`$(echo ${RESPONSE} | grep 'real' | wc -1)`
+
+# 	if [ ${UP_COUNT} -ge 1 ]
+# 	then
+
+# 		echo "COMPLETE HEALTH CHECK"
+# 		switch_proxy
+# 		break;
+# 	else
+# 		echo " CHECKING HEALTH ..."
+# 		echo " HEALTH CHECK : ${RESPONSE}"
+# 	fi
+
+# 	if [ ${RETRY_COUNT} -eq 10 ]
+# 	then 
+# 		echo "HEALTH CHECK FAILED..."
+# 		echo "SUSPEND! "
+# 		exit 1
+# 	fi	
+# 		echo "> NOW HEALTH CHECKING >>>>"
+# 		sleep 10
+# done
+
+
+#!/usr/bin/env bash
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
@@ -6,34 +48,33 @@ source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "HEALTH CHECK START..."
-echo "IDLE_PORT : $IDLE_PORT"
-echo "curl -s http://localhost:$IDLE_PORT/profile"
-
+echo "> Health Check Start!"
+echo "> IDLE_PORT: $IDLE_PORT"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-	RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-	UP_COUNT=`$(echo ${RESPONSE} | grep 'real' | wc -1)`
+  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
-	if [ ${UP_COUNT} -ge 1 ]
-	then
+  if [ ${UP_COUNT} -ge 1 ]
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
+  fi
 
-		echo "COMPLETE HEALTH CHECK"
-		switch_proxy
-		break;
-	else
-		echo " CHECKING HEALTH ..."
-		echo " HEALTH CHECK : ${RESPONSE}"
-	fi
+  if [ ${RETRY_COUNT} -eq 10 ]
+  then
+    echo "> Health check 실패. "
+    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
 
-	if [ ${RETRY_COUNT} -eq 10 ]
-	then 
-		echo "HEALTH CHECK FAILED..."
-		echo "SUSPEND! "
-		exit 1
-	fi	
-		echo "> NOW HEALTH CHECKING >>>>"
-		sleep 10
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
 done
